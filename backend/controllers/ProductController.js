bc6b10e838af66e7d54cfcbdda61ce644f303ea3
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2023-09-23 19:16:16 -0300

    editProduct, getAllProducts, GetProductById, deleteProduct

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,21 +1,28 @@
+const {Op} = require('sequelize')
 const Product = require('../models/Product')
 const Supermarket = require('../models/Supermarket')
 const {validationResult} = require('express-validator')
 const getToken = require('../helpers/getToken')
 const getUserByToken = require('../helpers/getUserByToken')
+
+function generateNumber(){
+    const min = 10**12
+    const max = 10**13 -1
+    return Math.floor(Math.random() * (max-min + 1) + min)
+}
 module.exports = class ProductController{
     static async create(req, res){
-        const {name, barcode, price, qty} = req.body
+        const {name, price, qty} = req.body
         const errors = validationResult(req)
         if(!errors.isEmpty()){
             res.status(422).json({message: errors})
             return
         }
         const user = await getUserByToken(req, res)
-        console.log(user)
+        const randomNumber = generateNumber()
         const product = {
             name: name,
-            barcode: barcode,
+            barcode: randomNumber.toString(),
             price: price,
             qty: qty,
             SupermarketId: user.id
@@ -27,6 +34,78 @@ module.exports = class ProductController{
             res.status(422).json({message: error})
             return
         }
+    }
+    static async delete(req, res){
+        const id = req.params.id
+        try{
+            const productExists = await Supermarket.findOne({where: {id: id}})
+            if(!productExists){
+                res.status(422).json({message: "Produto não encontrado!"})
+                return
+            }
+            await Product.destroy({where: {id: id}})
+            res.status(200).json({message: "Produto excluído!"})
+        }catch(error){
+            res.status(422).json({message: error})
+        }
+    }
+    static async editProduct(req, res){
+        const id = req.params.id
+        const {name, price, qty, barcode} = req.body
         
+        const product = {
+            name: name,
+            barcode: barcode,
+            price: price,
+            qty: qty
+        }
+
+        try{
+            await Product.update(product, {where: {id: id}})
+            res.status(200).json({message: 'Produto atualizado!'})
+        }catch(error){
+            res.status(422).json({message: error})
+        }
+    }
+    static async getAllProducts(req, res){
+        const products = await Product.findAll()
+        try{
+            res.status(200).json({message: {products}})
+        }catch(error){
+            res.status(422).json({message: error})
+        }
+    }
+    static async getProductById(req, res){
+        const id = req.params.id
+        const product = await Product.findOne({where: {id: id}})
+        if(!product){
+            res.status(422).json({message: 'Produto não encontrado!'})
+            return
+        }
+        try{
+            res.status(200).json({message: product})
+        }catch(error){
+            res.status(422).json({message: error})
+        }
+    }
+    static async getProductsByName(req, res){
+        const {q} = req.query.search
+        console.log(q)
+        const products = await Product.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${q}%`
+                }
+            }
+        })
+        if(products.length === 0){
+            res.status(422).json({message: 'Produto não encontrado!'})
+            return
+        }
+        try{
+            res.status(200).json({message: {products}})
+        }catch(error){
+            res.status(422).json({message: error})
+        }
     }
 }
--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -6,9 +6,13 @@ const verifyToken = require('../helpers/verifyToken')
 
 router.post('/create', [
     body('name').notEmpty().withMessage('O campo NOME não pode ser vazio'),
-    body('barcode').notEmpty().withMessage('O campo CÓDIGO DE BARRAS não pode ser vazio'),
     body('price').notEmpty().withMessage('O campo PREÇO não pode ser vazio'),
     body('qty').notEmpty().withMessage('O campo QUANTIDADE não pode ser vazio'),
 ],verifyToken, ProductController.create)
+router.delete('/delete/:id', verifyToken, ProductController.delete)
+router.patch('/edit/:id', verifyToken, ProductController.editProduct)
+router.get('/all', verifyToken, ProductController.getAllProducts)
+router.get('/:id', verifyToken, ProductController.getProductById)
+router.get('/?q=', verifyToken, ProductController.getProductsByName)
 
 module.exports = router
